79f9439ec662699fa201dfdd11f415e874c38935
Derek Xu <xzrderek@gmail.com>
2020-07-31 00:13:35 -0700

    updated register and login pages, added to db

--- routes/users.js
+++ routes/users.js
@@ -3,18 +3,6 @@ var router = express.Router();
 const passport = require('passport');
 const utils = require('../utils')
 
-router.get('/login', (req, res, next) => {
-  const errors = req.flash().error || [];
-  res.render('login', { errors });
-});
-
-router.post('/login', passport.authenticate('local', {
-  failureFlash: true,
-  failureRedirect: '/login',
-}), (req, res, next) => {
-  res.redirect('/posts'); //after registering, you are taken to all posts
-});
-
 router.get('/register', (req, res, next) => {
   const flash = req.flash();
   const error = flash.error || [];
@@ -24,20 +12,32 @@ router.get('/register', (req, res, next) => {
 });
 
 router.post('/register', (req, res, next) => {
-  const { username, password } = req.body;
+  const { firstName, lastName, email, type, username, password } = req.body;
   const users = req.app.locals.users;
   const hashedPassword = utils.hashPassword(password);
 
   users
-    .insertOne({ username, password: hashedPassword })
+    .insertOne({ firstName, lastName, email, type, username, password: hashedPassword })
     .then(() => {
-      req.flash('success', 'User registered successfully')
-      res.redirect('/register');
+      req.flash('success', 'Successfully signed up! Nice to meet you ' + req.body.firstName)
+      res.redirect('/login'); //now log in, never actually see the flash because taken to diff page
     })
     .catch(() => {
-      req.flash('error', 'Error registering user');
+      req.flash('error', 'Error registering user, try again!');
       res.redirect('/register');
     });
+});
+
+router.get('/login', (req, res, next) => {
+  const errors = req.flash().error || [];
+  res.render('login', { errors });
+});
+
+router.post('/login', passport.authenticate('local', {
+  failureFlash: true,
+  failureRedirect: '/login',
+}), (req, res, next) => {
+  res.redirect('/profile'); //after registering, you are taken to the profile page, STILL NEED TO MAKE
 });
 
 module.exports = router;
